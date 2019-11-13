a77e3016df46fec96a69bb892cd9bdc24e5f51b1
w1mbleb1mble <56442533+w1mbleb1mble@users.noreply.github.com>
2019-11-12 21:36:08 -0500

    Update Jack.cpp

--- Jack.cpp
+++ Jack.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-	cout << "hello world" << endl;
-	return 0;
+    int a;
+    cout << "how many elements in your array?" << endl;
+    cin >> a;
+    int b[a];
+    for(int c=0; c<a; c++)
+    {
+        int d;
+        cout << "enter array element " << c << endl;
+        cin >> d;
+        b[c] = d;
+    }
+    return 0;
 }
